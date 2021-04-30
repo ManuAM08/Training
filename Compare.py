ecf72747ef5d52bcad78a3d0824776949af2522b
ManuAM08 <milna106@gmail.com>
2021-04-30 13:39:39 +0530

    New project1

---
+++ Compare.py
@@ -0,0 +1,16 @@
+x=int(input("value of x is:"))
+y=int(input("value of y is:"))
+z=int(input("value of z is:"))
+if(x<y):
+    a=x
+elif(x>y):
+    a=y
+if(a<z):
+ print("Smaller value is",a)
+elif(a>z):
+ print("Smaller value is",z)
+
+
+
+
+
