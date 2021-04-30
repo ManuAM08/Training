6089b1a3568eec955391db934751a706b21ece8f
ManuAM08 <milna106@gmail.com>
2021-04-30 13:47:18 +0530

    Comparison

---
+++ Compare.py
@@ -0,0 +1,13 @@
+x=int(input("Enter the value of x:"))
+y=int(input("Enter the value of y:"))
+z=int(input("Enter the value of z:"))
+if(x<y):
+    a=x
+elif(x>y):
+    a=y
+if(a<z):
+ print("Smaller value is", a)
+elif(a>z):
+ print("Smaller value is", z)
+
+
